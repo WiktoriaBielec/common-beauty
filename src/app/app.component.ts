748900de2e01a86d0c819ca1e2c6c6bb9a72f415
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2024-04-27 21:12:22 +0200

    Added first view component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstViewComponent } from "./components/first-view/first-view.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, FirstViewComponent]
 })
 export class AppComponent {
   title = 'common-beauty';
