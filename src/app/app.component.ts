98e1fe108b029ab25d5c681ba769029d0ea572bd
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2024-05-07 11:06:33 +0200

    Created basic fetures of first view and about sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstViewComponent } from "./components/first-view/first-view.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, FirstViewComponent]
+    imports: [RouterOutlet, FirstViewComponent, NavigationComponent, AboutUsComponent]
 })
 export class AppComponent {
   title = 'common-beauty';
