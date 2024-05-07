0cf1841095b64486b2e5ee38cbf1be3c1505eca6
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2024-05-07 14:38:07 +0200

    Added our-vision mobile version component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FirstViewComponent } from "./components/first-view/first-view.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { OurVisionComponent } from "./components/our-vision/our-vision.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, FirstViewComponent, NavigationComponent, AboutUsComponent]
+    imports: [RouterOutlet, FirstViewComponent, NavigationComponent, AboutUsComponent, OurVisionComponent]
 })
 export class AppComponent {
   title = 'common-beauty';
