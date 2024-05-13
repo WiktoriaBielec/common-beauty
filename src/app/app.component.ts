b21fbbda1ba24d8dea2fa4ba3abb3a1a1c8ac704
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2024-05-13 17:51:19 +0200

    card component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,14 @@ import { FirstViewComponent } from "./components/first-view/first-view.component
 import { NavigationComponent } from "./components/navigation/navigation.component";
 import { AboutUsComponent } from "./components/about-us/about-us.component";
 import { OurVisionComponent } from "./components/our-vision/our-vision.component";
-import { GreyButtonComponent } from "./components/generics/grey-button/grey-button.component";
-import { TransparentButtonComponent } from "./components/generics/transparent-button/transparent-button.component";
+import { CardComponent } from "./components/generics/card/card.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, FirstViewComponent, NavigationComponent, AboutUsComponent, OurVisionComponent, GreyButtonComponent, TransparentButtonComponent]
+    imports: [RouterOutlet, FirstViewComponent, NavigationComponent, AboutUsComponent, OurVisionComponent, CardComponent]
 })
 export class AppComponent {
   title = 'common-beauty';
